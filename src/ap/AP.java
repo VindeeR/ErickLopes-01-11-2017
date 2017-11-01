c9a6bbd00bb439bb505d6e80cdc68e142c1f7c6f
31664822 <31664822@087829-P33S103.macklabs.br>
2017-11-01 11:21:44 -0200

    inclusão da variavel x

--- src/ap/AP.java
+++ src/ap/AP.java
@@ -15,7 +15,7 @@ public class AP {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        int x = 10;
     }
     
 }
